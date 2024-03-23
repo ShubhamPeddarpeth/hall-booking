a1d37082c16edc2fb9198dcca22c2aab2a8c2473
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-03-23 12:34:10 +0530

    booking update

--- routes/bookings.js
+++ routes/bookings.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res, next) => {
   try {
     const bookings = await Booking.find();
     res.json(bookings);
-    console.log('booking')
+    console.log("booking");
   } catch (error) {
     next(error);
   }
@@ -17,6 +17,41 @@ router.get("/", async (req, res, next) => {
 // Create a new booking
 router.post("/", async (req, res, next) => {
   try {
+    const { roomId, date, startTime, endTime } = req.body;
+
+    // Check if the room is already booked for the same date and time
+    const existingBooking = await Booking.findOne({
+      roomId: roomId,
+      date: date,
+      $or: [
+        {
+          $and: [
+            { startTime: { $lte: startTime } },
+            { endTime: { $gt: startTime } },
+          ],
+        },
+        {
+          $and: [
+            { startTime: { $lt: endTime } },
+            { endTime: { $gte: endTime } },
+          ],
+        },
+        {
+          $and: [
+            { startTime: { $gte: startTime } },
+            { endTime: { $lte: endTime } },
+          ],
+        },
+      ],
+    });
+
+    if (existingBooking) {
+      return res
+        .status(400)
+        .json({ message: "Room already booked for the same date and time." });
+    }
+
+    // If room is available, create the booking
     const booking = await Booking.create(req.body);
     res.status(201).json(booking);
   } catch (error) {
